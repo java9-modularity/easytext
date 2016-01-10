fb7d8143ec01cb6aa44c8efad080910888cac280
Sander Mak <sander.mak@luminis.eu>
2016-01-10 15:14:57 +0100

    Wrapper class implemented

--- src/javamodularity.easytext.algorithm.nextgensyllablecounter/javamodularity/easytext/algorithm/nextgensyllablecounter/SyllableCounter3Wrapper.java
+++ src/javamodularity.easytext.algorithm.nextgensyllablecounter/javamodularity/easytext/algorithm/nextgensyllablecounter/SyllableCounter3Wrapper.java
@@ -4,9 +4,12 @@ import javamodularity.easytext.algorithm.api.SyllableCounter;
 
 public class SyllableCounter3Wrapper implements SyllableCounter {
 
+   eu.crydee.syllablecounter.SyllableCounter counter = new 
+      eu.crydee.syllablecounter.SyllableCounter(1000);
+
    @Override
    public int countSyllables(String word) {
-      return 1;
+      return counter.count(word);
    }
    
 }
--- src/javamodularity.easytext.algorithm.nextgensyllablecounter/module-info.java
+++ src/javamodularity.easytext.algorithm.nextgensyllablecounter/module-info.java
@@ -1,6 +1,6 @@
 module javamodularity.easytext.algorithm.nextgensyllablecounter {
   requires javamodularity.easytext.algorithm.api;
-  requires syllablecounter3;
+  requires syllablecounter3; // Automatic module: name derived from library jar name
 
   provides javamodularity.easytext.algorithm.api.SyllableCounter 
     with javamodularity.easytext.algorithm.nextgensyllablecounter.SyllableCounter3Wrapper;
