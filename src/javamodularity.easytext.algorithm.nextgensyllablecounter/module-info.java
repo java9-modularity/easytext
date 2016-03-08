8e89d1e10c5df470c02e582efaaccdec9560cd44
Sander Mak <sander.mak@luminis.eu>
2016-03-08 15:24:43 +0100

    Use original jar name

--- src/javamodularity.easytext.algorithm.nextgensyllablecounter/module-info.java
+++ src/javamodularity.easytext.algorithm.nextgensyllablecounter/module-info.java
@@ -1,6 +1,6 @@
 module javamodularity.easytext.algorithm.nextgensyllablecounter {
   requires javamodularity.easytext.algorithm.api;
-  requires syllablecounter3; // Automatic module: name derived from library jar name
+  requires syllable.counter; // Automatic module: name derived from library jar name
 
   provides javamodularity.easytext.algorithm.api.SyllableCounter 
     with javamodularity.easytext.algorithm.nextgensyllablecounter.SyllableCounter3Wrapper;
