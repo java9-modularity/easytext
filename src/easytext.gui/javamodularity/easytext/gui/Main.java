bd6d1e3644043330992fce0f1ca5cb9823638f1e
Sander Mak <sander.mak@luminis.eu>
2016-09-06 11:27:52 +0200

    Resolve conflict

--- src/easytext.gui/javamodularity/easytext/gui/Main.java
+++ src/easytext.gui/javamodularity/easytext/gui/Main.java
@@ -17,19 +17,20 @@ import javafx.stage.Stage;
 
 import javamodularity.easytext.algorithm.api.Analyzer;
 import static javamodularity.easytext.algorithm.api.Preprocessing.toSentences;
- 
+
 public class Main extends Application {
 
     private static Iterable<Analyzer> analyzers;
     private static ComboBox<String> algorithm;
     private static TextArea input;
     private static Text output;
+    private static String sampleText;
 
     public static void main(String[] args) {
         analyzers = ServiceLoader.load(Analyzer.class);
         launch(args);
     }
-    
+
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("EasyText");
@@ -38,21 +39,22 @@ public class Main extends Application {
         btn.setOnAction(event ->
           output.setText(analyze(input.getText(), (String) algorithm.getValue()))
         );
-        
+
         VBox vbox = new VBox();
         vbox.setPadding(new Insets(3));
         vbox.setSpacing(3);
         Text title = new Text("Choose an algorithm:");
         algorithm = new ComboBox<>();
         for(Analyzer analyzer: analyzers) {
-            algorithm.getItems().add(analyzer.getName());    
+            algorithm.getItems().add(analyzer.getName());
         }
-        
+
         vbox.getChildren().add(title);
         vbox.getChildren().add(algorithm);
         vbox.getChildren().add(btn);
-        
+
         input = new TextArea();
+        input.setText(sampleText);
         output = new Text();
         BorderPane pane = new BorderPane();
         pane.setRight(vbox);
@@ -72,4 +74,11 @@ public class Main extends Application {
         return "No analyzer found for " + algorithm;
     }
 
+    static {
+      try {
+        sampleText = new String (java.nio.file.Files.readAllBytes(java.nio.file.Paths.get("testinput.txt")));
+      } catch (java.io.IOException ioe) {
+        sampleText = "Paste text here";
+      }
+    }
 }
